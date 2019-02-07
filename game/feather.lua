9810ebab4a64782d7d7a529c2c9dd29c70fc0cef
Federico del Mazo <fdelmazo@fi.uba.ar>
2019-02-07 18:19:34 -0300

    Feather image placeholder; better physics

--- game/feather.lua
+++ game/feather.lua
@@ -2,50 +2,41 @@ local feather = {}
 
 local window_width, window_height, _ = love.window.getMode( )
 
-local feather_width = 180
-local feather_height = 60
+
+local feather_image = love.graphics.newImage( "assets/Feather.png" )
+local feather_width = feather_image:getWidth()
+local feather_height = feather_image:getHeight()
 local feather_x = (window_width/2)-(feather_width/2)
 local feather_y = (window_height*2/3)-(feather_height/2)
- 
-local piso = window_height - feather_height/2
-local techo = -feather_height/2
 
-local ascent = false
+
+local floor = window_height + feather_height
+local roof = -feather_height/2
+
+local velocity = 0
+local acceleration = -100
 
 function feather:enter()
   love.graphics.setBackgroundColor( 0,0,0 )
 end
 
 function feather:update(dt)
-  descend(dt)
-  if ascent then ascend(dt) end
+  -- descend(dt)
+  if love.keyboard.isDown('space') then
+    velocity = velocity - 5
+	end
+
+  if not (velocity == 0) then
+		feather_y = feather_y + velocity * dt
+		velocity = velocity - acceleration * dt
+  end
+  
+  if feather_y > floor then feather_y = floor end
 end
 
 function feather:draw()
   love.graphics.setColor( 214,214,0,255)
-  love.graphics.rectangle("fill", feather_x, feather_y, feather_width, feather_height )
-end
-
-function descend(dt) 
-  feather_y = feather_y + (dt*100)
-  if feather_y > piso then feather_y = piso end
-end
-
-function ascend(dt) 
-  feather_y = feather_y - (dt*200)
-  if feather_y < techo then feather_y = techo end
-end
-
-function feather:keypressed(key)
-  if key=='space' then
-    ascent = true
-  end
-end
-
-function feather:keyreleased(key)
-  if key=='space' then
-    ascent = false
-  end
+  love.graphics.draw(feather_image, feather_x, feather_y)
 end
 
 return feather
