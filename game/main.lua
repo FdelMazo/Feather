eba3836c476e9dac831e3b4523209cd4b1bcb0f1
Federico del Mazo <fdelmazo@fi.uba.ar>
2019-02-08 20:18:24 -0300

    Wow so secret!

--- game/main.lua
+++ game/main.lua
@@ -3,6 +3,8 @@ Gamestate = require("libs/gamestate")
 window_width, window_height = love.window.getMode()
 font = love.graphics.newFont("assets/Pixel UniCode.ttf", 32)
 local bgm = love.audio.newSource("assets/environments.mp3", "stream")
+local bgm2 = love.audio.newSource("assets/wow_so_secret.mp3", "stream")
+local secret = true
 
 feather = require("objects/feather")
 rain = require("objects/rain")
@@ -22,10 +24,14 @@ function love.load()
   Gamestate.switch(intro) 
 end
 
+function love.update(dt)
+  if bgm:isStopped() and secret then love.audio.play(bgm2) ; secret = false end
+end
+
 function love.keyreleased(key)
   if key=='m' then
     if bgm:isPlaying() then love.audio.pause( bgm )
-    else love.audio.play( bgm )
+    else love.audio.play(bgm)
     end
   end
   
